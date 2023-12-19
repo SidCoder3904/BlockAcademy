c1ea3f8ab48c26df47b676dd1d9c3ca2bcb664ed
Siddharth Verma <siddharthverma3904@gmail.com>
2023-12-20 00:25:45 +0530

    made structures for student/staff info

--- contract.sol
+++ contract.sol
@@ -1,58 +1,87 @@
 // SPDX-License-Identifier: MIT 
-pragma solidity >=0.7.4;
+pragma solidity >0.7.0 <=0.8.23;
 
-contract student_ {
-    enum subjects {MAT, ENG, SCI, SST, COM}
-    address admin ;
-    
-    uint256 internal constant nSubj = 5;
+contract School {
 
-    string public first_name = "Firstname";
-    string public last_name = "Lastname";
-    uint256 public age = 18;
-    bool public isPass;
-   mapping (address=>mapping(subjects => uint256)) internal marks;
-    mapping (subjects => address) internal  prof;
-    mapping (string => mapping(string => address)) internal student;
-    
-    constructor(){
-       
-        prof[subjects.MAT]=0xdD870fA1b7C4700F2BD7f44238821C26f7392148;
-        prof[subjects.ENG]=0x583031D1113aD414F02576BD6afaBfb302140225;
-        prof[subjects.SCI]=0x4B0897b0513fdC7C541B6d9D7E929C4e5364D2dB;
-        prof[subjects.SST]=0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C;
-        prof[subjects.COM]=0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
-        admin=0x0A098Eda01Ce92ff4A4CCb7A4fFFb5A43EBC70DC;
-    }
+    mapping(string => uint32) public course_codes;    // maps string course code to number, can be edited only by admin
 
-    modifier onlyProfessor(subjects sub_code) {
+    // ************* custom structures to store students/staff info ****************
 
-        require(msg.sender == prof[sub_code]);
-        _;
-    }
-    modifier onlyAdmin{
-        require (msg.sender == admin);
-        _;
-    }
-    modifier onlystudent(string memory _fname,string memory _lname){
-        require(student[_fname][_lname] == msg.sender);
-        _;
+    struct course {
+        uint32 course_code;    // each subject/course will have unique code
+        uint256 marks;    // can be extended to part wise marks like midsem, endsem, etc
     }
     
+    struct grade {
+        uint8 class;    // class like 1,2,...,12 or sem like 1,2,...,8  
+        course[] courses;
+        uint8 n_courses;
+    }
 
-    /*constructor(string memory _fname, string memory _lname, uint256 _age) {
-        first_name = _fname;
-        last_name = _lname;
-        age = _age;
-        isPass = true;
-        for(uint256 i=0; i<nSubj; i++) marks[i] = 100;
-    }*/
+    struct Student {
+        address acct;
+        string first_name;
+        string last_name;
+        uint32 inst_id;     // students will have their unique identity
+        grade[] grades;
+        uint8 n_grades;
+        // can add more personal details like dob, parents, contact, address etc
+    }
 
+    struct program {
+        uint32 course_code;
+        uint16 max_strenght;
+        // we can add constraints like eligibility criterian etc...
+    }
+
+    struct Instructor {
+        address acct;
+        string first_name;
+        string last_name;
+        uint32 staff_id;    // staff will have unique id
+        program[] programs_offered;  // offered by this prof
+        uint8 n_programs;
+        // can add more personal info like contact, degree etc...
+    }
+
+    string public school_name;
+    address internal admin;      // can validate and unenroll students/staff in case situation arises but enrollment is decentralized by student themselves
+    mapping(uint32 => Student) internal all_students;
+    mapping(uint32 => Instructor) internal all_instructors;
+
+    // ****************** constructors and modifiers to manage access *********************
+
+    constructor(string memory _school_name) {     // initialize school
+        school_name = _school_name;
+        admin = msg.sender;
+    }
+
+    modifier onlyAdmin {
+        require(msg.sender == admin);
+        _;
+    }
+
+    modifier onlyProfessor(uint32 _staff_id, string memory _course_name) {
+        require(msg.sender == all_instructors[_staff_id].acct);      // should be a valid instructor
+        bool valid_course = false;
+        for(uint8 i=0; i<all_instructors[_staff_id].n_programs; i++)    // should be taking the course
+            if(course_codes[_course_name] == all_instructors[_staff_id].programs_offered[i].course_code) valid_course = true;
+        require(valid_course == true);
+        _;
+    }
+
+    modifier onlyStudent(uint32 _inst_id){
+        require(msg.sender == all_students[_inst_id].acct);
+        _;
+    }
+
+    // *************** functions to access student record ********************
+    // to be edited...
     function Register(string memory _fname,string memory _lname,address _address) public onlyAdmin{
          student[_fname][_lname]=_address;
     }
 
-function editName(string memory _fname, string memory _lname) public {
+    function editName(string memory _fname, string memory _lname) public {
         first_name = _fname;
         last_name = _lname;
     }
