b5cdb58a82da48a7a6948747cd90c3e04b7f2769
Siddharth Verma <siddharthverma3904@gmail.com>
2023-12-21 23:02:40 +0530

    fixed registeration errors

--- contract.sol
+++ contract.sol
@@ -3,7 +3,7 @@ pragma solidity >0.7.0 <=0.8.23;
 
 contract School {
 
-    mapping(string => uint32) public course_codes;    // maps string course code to number, can be edited only by admin
+    mapping(string => uint32) internal course_codes;    // maps string course code to number, can be edited only by admin
 
     // ************* custom structures to store students/staff info ****************
 
@@ -22,7 +22,7 @@ contract School {
         address acct;   // maybe we can remove it
         string first_name;
         string last_name;
-        uint32 inst_id;     // students will have their unique identity
+        uint32 stud_id;     // students will have their unique identity
         uint8 n_grades;
         grade[] grades;
         // can add more personal details like dob, parents, contact, address etc
@@ -34,7 +34,7 @@ contract School {
         // we can add constraints like eligibility criterian etc...
     }
 
-    struct Instructor {
+    struct Staff {
         address acct;   // maybe we can remove it
         string first_name;
         string last_name;
@@ -44,14 +44,14 @@ contract School {
         // can add more personal info like contact, degree etc...
     }
 
-    uint32 school_strenght = 0;
-    uint32 staff_strenght = 0;
+    uint32 internal school_strenght = 999;   // to get only 4 digit ids
+    uint32 internal staff_strenght = 999;    // to get only 4 digit ids
     string public school_name;
     address internal admin;      // can validate and unenroll students/staff in case situation arises but enrollment is decentralized by student themselves
     
-    mapping(uint32 => Student) internal all_students;   // inst_id -> student info
-    mapping(address => uint32) internal student_map;    // acct adrs -> inst_id
-    mapping(uint32 => Instructor) internal all_instructors;     // staff_id -> staff info
+    mapping(uint32 => Student) internal all_students;   // stud_id -> student info
+    mapping(address => uint32) internal student_map;    // acct adrs -> stud_id
+    mapping(uint32 => Staff) internal all_staff;     // staff_id -> staff info
     mapping(address => uint32) internal staff_map;      // acct adrs -> staff_id
 
     // ****************** constructors and modifiers to manage access *********************
@@ -66,12 +66,22 @@ contract School {
         _;
     }
 
-    modifier onlyStaff(uint32 _staff_id, string memory _course_name) {
-        require(msg.sender == all_instructors[_staff_id].acct);      // should be a valid instructor
-        bool valid_course = false;
-        for(uint8 i=0; i<all_instructors[_staff_id].n_programs; i++)    // should be taking the course
-            if(course_codes[_course_name] == all_instructors[_staff_id].programs_offered[i].course_code) valid_course = true;
-        require(valid_course == true);
+    modifier onlyStaff(uint32 _staff_id) {
+        require(msg.sender == all_staff[_staff_id].acct);      // should be a valid instructor
+        // bool valid_course = false;
+        // for(uint8 i=0; i<all_staff[_staff_id].n_programs; i++)    // should be taking the course
+        //     if(course_codes[_course_name] == all_staff[_staff_id].programs_offered[i].course_code) valid_course = true;
+        // require(valid_course == true);
+        _;
+    }
+
+    modifier validStaffId(uint32 _staff_id) {
+        require(_staff_id <= staff_strenght && _staff_id > 999);
+        _;
+    }
+
+    modifier validStudId(uint32 _stud_id) {
+        require(_stud_id <= school_strenght && _stud_id > 999);
         _;
     }
 
@@ -93,33 +103,48 @@ contract School {
     // *************** functions to access student record ********************
     // to be edited...
     // functions to be made:
-    // register, enroll, edit details, setmarks, getmarks, add courses, unregister
+    // register, enroll, get details, edit details, setmarks, getmarks, add courses, unregister
+
+    function GetSchoolStrenght() public view returns(uint32) {
+        return school_strenght-999;
+    }
+
+    function GetStaffStrenght() public view returns(uint32) {
+        return staff_strenght-999;
+    }
+
     function RegisterStudent(string memory _fname, string memory _lname) public notStudent notStaff returns(uint32) {   // staff also cannot register as student for now
-        uint32 inst_id = ++school_strenght;
-        student_map[msg.sender] = inst_id;
-        Student memory new_student;
-        new_student.acct = msg.sender;
-        new_student.first_name = _fname;
-        new_student.last_name = _lname;
-        new_student.inst_id = inst_id;
-        new_student.n_grades = 0;
-        all_students[inst_id] = new_student;
-        return inst_id;
+        uint32 stud_id = ++school_strenght;
+        student_map[msg.sender] = stud_id;
+        all_students[stud_id].acct = msg.sender;
+        all_students[stud_id].first_name = _fname;
+        all_students[stud_id].last_name = _lname;
+        all_students[stud_id].stud_id = stud_id;
+        all_students[stud_id].n_grades = 0;
+        return stud_id;
         // add time lock so that someone else might not be able to edit the blockchain at same time
+    }
+
+    function GetStudentDetails(uint32 _stud_id) public view validStudId(_stud_id) onlyStudent(_stud_id) returns(string memory, string memory) {
+        return (all_students[_stud_id].first_name, all_students[_stud_id].last_name);
     }
 
     function RegisterStaff(string memory _fname, string memory _lname) public notStudent notStaff returns(uint32) {   // staff also cannot register as student for now
         uint32 staff_id = ++staff_strenght;
         staff_map[msg.sender] = staff_id;
-        Instructor memory new_staff;
-        new_staff.acct = msg.sender;
-        new_staff.first_name = _fname;
-        new_staff.last_name = _lname;
-        new_staff.staff_id = staff_id;
-        new_staff.n_programs = 0;
-        all_instructors[staff_id] = new_staff;
+        all_staff[staff_id].acct = msg.sender;
+        all_staff[staff_id].first_name = _fname;
+        all_staff[staff_id].last_name = _lname;
+        all_staff[staff_id].staff_id = staff_id;
+        program memory initial_prog;
+        all_staff[staff_id].programs_offered.push(initial_prog);
+        all_staff[staff_id].n_programs = 1;     // bcoz we have added 
         return staff_id;
         // add time lock so that someone else might not be able to edit the blockchain at same time
+    }
+
+    function GetStaffDetails(uint32 _staff_id) public view validStaffId(_staff_id) onlyStaff(_staff_id) returns(string memory, string memory) {
+        return (all_staff[_staff_id].first_name, all_staff[_staff_id].last_name);
     }
 
     // function editName(string memory _fname, string memory _lname) public {
