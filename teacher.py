ffd1690c998efe98aca5303188cbdd35e0e5e201
jyoti7777 <149932247+jyoti7777@users.noreply.github.com>
2024-01-20 22:06:00 +0530

    Update teacher.py

--- teacher.py
+++ teacher.py
@@ -1,32 +1,31 @@
 import streamlit as st
 import time
 import app
+import teacher_functions
+
+
+
 
 def teacher_page(contract):
     st.write("**Welcome to the Teacher page.**")
 
-    # Login Form for Teacher
+   
     user_address = st.text_input("Enter Ethereum Address:")
-    # password = st.text_input("Enter Password:", type="password")
+   
     login_button = st.button("Login")
 
     if login_button:
-        # You can integrate your authentication logic here
+        
         login_successful = authenticate_teacher(user_address, contract)
-
-        # Display login status
         if login_successful:
-            st.success("Login successful!")
-        else:
-            st.error("Login failed. Please check your credentials or Register")
-            time.sleep(2)
-            app.site = 'Register'
-
-def authenticate_teacher(user_address, contract):
-    # Implement your logic to authenticate the teacher
-    try :
-        staffID = contract.functions.GetStaffId().call({'from': str(user_address)})
-        print(staffID)
+            st.write("Login Successfull")
+            return user_address
+            
+def authenticate_teacher(user_address,contract):
+    try:
+        contract.functions.GetStaffId().call({'from': user_address})
         return True
-    except :
+    except:
         return False
+
+ 
